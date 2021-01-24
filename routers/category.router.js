e4ddb6040a9d28efeba50d1cf7a36a0a23ae2ed5
dragonitaq <kevincharlk@gmail.com>
2021-01-24 13:49:04 +0800

    Prepare for deployment. Fix incorrect controllers folder name.

--- routers/category.router.js
+++ routers/category.router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { categoryController } = require('../Controllers/category.controller');
+const { categoryController } = require('../controllers/category.controller');
 
 const router = express.Router();
 
